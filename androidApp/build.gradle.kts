52dba1833326db2ef668e160f72155da23787b67
masud <sayed.masud@lead-bd.com>
2024-04-08 13:25:05 +0600

    gradle implement for andriod

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -45,4 +45,40 @@ dependencies {
     implementation(libs.compose.material3)
     implementation(libs.androidx.activity.compose)
     debugImplementation(libs.compose.ui.tooling)
+
+//    val composeBom = platform("androidx.compose:compose-bom:2022.10.00")
+    implementation ("androidx.core:core-ktx:1.12.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation ("androidx.activity:activity-compose:1.8.2")
+    implementation ("androidx.compose.foundation:foundation:1.5.4")
+    implementation ("androidx.compose.material:material:1.5.4")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation ("com.google.accompanist:accompanist-pager:0.30.0")
+    implementation ("com.google.accompanist:accompanist-pager-indicators:0.30.0")
+
+    implementation ("com.google.android.material:material:1.10.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    //noinspection GradleDependency
+    implementation ("androidx.navigation:navigation-compose:2.7.4")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.9.0"))
+
+    implementation ("io.github.vanpra.compose-material-dialogs:datetime:0.9.0")
+    coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:2.0.3")
+
+    implementation ("io.coil-kt:coil-compose:2.4.0")
+    implementation ("io.coil-kt:coil:2.4.0")
+
+    //biometric finger print
+    implementation("androidx.biometric:biometric:1.2.0-alpha05")
+
+    //Stepper implementation
+    implementation("androidx.compose.ui:ui:1.6.0")
+    implementation ("androidx.compose.material:material:1.5.4")
+    implementation ("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+    implementation ("androidx.datastore:datastore-preferences:1.0.0")
+    implementation ("com.airbnb.android:lottie-compose:6.1.0")
 }
