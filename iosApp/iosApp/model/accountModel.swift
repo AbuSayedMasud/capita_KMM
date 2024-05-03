7762e5e881751c082aec511d434dad69e7042494
Tipu-leads <tipu.diu.cse@gmail.com>
2024-05-04 02:11:27 +0600

    new accountmodel added and ios mockloader updated
    new accountmodel added and ios mockloader updated

--- iosApp/iosApp/model/accountModel.swift
+++ iosApp/iosApp/model/accountModel.swift
@@ -28,6 +28,25 @@ struct iOSAccountBalance: Codable {
     let totalDeposit: Double
     let totalWithdrawal: Double
     let unclearCheque: Double
+    let deposit: Double
+    let dividendIncome: Double
+    let dividendReceivable: Double
+    let fundTransIn: Double
+    let fundTransOut: Double
+    let fundWithdrawalRequest: Double
+    let instrumentTransIn: Double
+    let instrumentTransOut: Double
+    let ipoApplication: Double
+    let maxLoan: Double
+    let netDepositWithdraw: Double
+    let netGainLoss: Double
+    let preIpoApplication: Double
+    let preferenceApplication: Double
+    let realiseGainLoss: Double
+    let rightApplication: Double
+    let rightOrder: Double
+    let unrealiseGainLoss: Double
+    let withdrawal: Double
     
     // Initialize iOSAccountBalance from shared AccountBalance
     init(sharedBalance: AccountBalance) {
@@ -48,5 +67,26 @@ struct iOSAccountBalance: Codable {
         self.totalDeposit = sharedBalance.totalDeposit
         self.totalWithdrawal = sharedBalance.totalWithdrawal
         self.unclearCheque = sharedBalance.unclearCheque
+        
+        // Assign default values for new fields
+        self.deposit = sharedBalance.deposit
+        self.dividendIncome = sharedBalance.dividendIncome
+        self.dividendReceivable = sharedBalance.dividendReceivable
+        self.fundTransIn = sharedBalance.fundTransIn
+        self.fundTransOut = sharedBalance.fundTransOut
+        self.fundWithdrawalRequest = sharedBalance.fundWithdrawalRequest
+        self.instrumentTransIn = sharedBalance.instrumentTransIn
+        self.instrumentTransOut = sharedBalance.instrumentTransOut
+        self.ipoApplication = sharedBalance.ipoApplication
+        self.maxLoan = sharedBalance.maxLoan
+        self.netDepositWithdraw = sharedBalance.netDepositWithdraw
+        self.netGainLoss = sharedBalance.netGainLoss
+        self.preIpoApplication = sharedBalance.preIpoApplication
+        self.preferenceApplication = sharedBalance.preferenceApplication
+        self.realiseGainLoss = sharedBalance.realiseGainLoss
+        self.rightApplication = sharedBalance.rightApplication
+        self.rightOrder = sharedBalance.rightOrder
+        self.unrealiseGainLoss = sharedBalance.unrealiseGainLoss
+        self.withdrawal = sharedBalance.withdrawal
     }
 }
