aed6d2374926e0fc1ec46c9865b49aed31993b2e
sayed.masud <sayed.masud@leads-bd.com>
2024-06-02 11:01:36 +0600

    service different screen add

--- androidApp/src/main/java/com/leads/capita/android/shell/ServiceNavGraph.kt
+++ androidApp/src/main/java/com/leads/capita/android/shell/ServiceNavGraph.kt
@@ -9,7 +9,10 @@ import androidx.navigation.compose.composable
 import com.leads.capita.android.service.ServiceScreen
 import com.leads.capita.android.service.deposit.DepositScreen
 import com.leads.capita.android.service.deposit.DepositStatusScreen
+import com.leads.capita.android.service.ipo.IpoScreen
 import com.leads.capita.android.service.payment.PaymentScreen
+import com.leads.capita.android.service.productSwitch.ProductSwitchScreen
+import com.leads.capita.android.service.taxCertificate.TaxCertificateScreen
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
@@ -20,6 +23,15 @@ fun ServiceNavGraph(navController: NavHostController) {
     ) {
         composable("services") {
             ServiceScreen(navController = navController)
+        }
+        composable("ipo") {
+            IpoScreen(navController = navController)
+        }
+        composable("taxCertificate") {
+            TaxCertificateScreen(navController = navController)
+        }
+        composable("productSwitch") {
+            ProductSwitchScreen(navController = navController)
         }
         composable("payment") {
             PaymentScreen(navController)
